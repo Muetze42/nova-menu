ee91e7c537594babae60ce20631e73456bcafec9
Norman Huth <norman@huth.it>
2023-10-12 15:20:28 +0200

    refactor: menu group nova 4.27.14 compatibility

--- resources/js/components/MenuGroup.vue
+++ resources/js/components/MenuGroup.vue
@@ -1,90 +1,94 @@
 <template>
-    <div class="sidebar-group" :class="item.classes.filterClass" v-if="item.items.length > 0">
-        <button
-            v-if="item.collapsable"
-            @click="handleClick"
-            class="sidebar-group-button mt-3"
-            :class="item.icons.classes.elem"
-            v-tooltip.click="item.tooltip"
-        >
-            <h4 class="sidebar-group-title">
-                <span class="sidebar-group-icon">
-                    <NHMenuIcon :icons="item.icons" v-if="!item.icons.asLabel" />
-                </span>
+  <div v-if="item.items.length > 0">
+    <h4
+      @click.prevent="handleClick"
+      class="flex items-center px-1 py-1 rounded text-left text-gray-500"
+      :class="{
+        'cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-800':
+          displayAsButton,
+        'font-bold text-primary-500 dark:text-primary-500': item.active,
+      }"
+      v-tooltip.click="item.tooltip"
+      @click="item.iframe.target ? open = true : null"
+    >
+      <span class="inline-block shrink-0 w-6 h-6">
+        <NHMenuIcon :icons="item.icons" v-if="!item.icons.asLabel"/>
+      </span>
 
-                <span class="sidebar-group-label flex items-center" :class="'gap-x-'+item.icons.labelGap">
-                    <NHMenuIcon :icons="item.icons" v-if="item.icons.asLabel" class="inline-block" />
-                    <span :class="item.icons.classes.label">
-                        {{ item.name }}
-                    </span>
-                </span>
+      <span
+        class="flex-1 flex items-center w-full tracking-wide uppercase font-bold text-left text-xs px-3 py-1"
+        :class="'gap-x-'+item.icons.labelGap"
+      >
+        <NHMenuIcon :icons="item.icons" v-if="item.icons.asLabel" class="inline-block"/>
+        {{ item.name }}
+      </span>
 
-                <CollapseButton
-                    class="sidebar-group-collapse-btn ml-auto"
-                    :collapsed="collapsed"
-                />
-            </h4>
-        </button>
-        <h4
-            v-else
-            class="sidebar-group-title"
-            :class="[item.icons.classes.elem, { 'cursor-pointer menu-iframe-group': item.iframe.target }]"
-            v-tooltip.click="item.tooltip"
-            @click="item.iframe.target ? open = true : null"
-        >
-            <span class="sidebar-group-icon">
-                <NHMenuIcon :icons="item.icons" v-if="!item.icons.asLabel" />
-            </span>
+      <span
+        v-if="item.collapsable"
+        class="inline-flex items-center justify-center shrink-0 w-6 h-6"
+      >
+        <CollapseButton :collapsed="collapsed" :to="item.path" />
+      </span>
+    </h4>
 
-            <span class="sidebar-group-label flex items-center" :class="'gap-x-'+item.icons.labelGap">
-                <NHMenuIcon :icons="item.icons" v-if="item.icons.asLabel" class="inline-block" />
-                <span :class="item.icons.classes.label">
-                    {{ item.name }}
-                </span>
-            </span>
-        </h4>
-
-        <template v-if="!collapsed && !item.iframe.target">
-            <component
-                :key="item.name"
-                v-for="item in item.items"
-                :is="item.component"
-                :item="item"
-            />
-        </template>
+    <div v-if="!collapsed">
+      <component
+        :key="item.name"
+        v-for="item in item.items"
+        :is="item.component"
+        :item="item"
+      />
     </div>
-    <div :class="item.iframe.wrapper.classes" :style="item.iframe.wrapper.styles" @click="open = false" v-if="open">
-        <iframe :class="item.iframe.iframe.classes" :style="item.iframe.iframe.styles" :src="item.iframe.target"></iframe>
-    </div>
+  </div>
+  <div :class="item.iframe.wrapper.classes" :style="item.iframe.wrapper.styles" @click="open = false" v-if="open">
+    <iframe :class="item.iframe.iframe.classes" :style="item.iframe.iframe.styles" :src="item.iframe.target"></iframe>
+  </div>
 </template>
 
 <script>
-import Collapsable from './../../../../../laravel/nova/resources/js/mixins/Collapsable.js'
+import Collapsable from './../../../../../vendor/laravel/nova/resources/js/mixins/Collapsable.js'
 
 export default {
-    name: "MenuGroup",
-    mixins: [Collapsable],
-    props: ['item'],
-    methods: {
-        handleClick() {
-            this.toggleCollapse()
-        },
+  mixins: [Collapsable],
+
+  props: ['item'],
+
+  methods: {
+    handleClick() {
+      if (this.item.collapsable) {
+        this.toggleCollapse()
+      }
     },
-    data() {
-        return {
-            open: false
-        };
+  },
+
+  data() {
+    return {
+      open: false
+    }
+  },
+  watch: {
+    open(isOpen) {
+      let body = document.querySelector('body')
+      body.style.overflow = isOpen ? 'hidden' : 'visible'
+    }
+  },
+
+  computed: {
+    component() {
+      if (this.item.items.length > 0) {
+        return 'div'
+      }
+
+      return 'h3'
     },
-    watch: {
-        open(isOpen) {
-            let body = document.querySelector('body')
-            body.style.overflow = isOpen ? 'hidden' : 'visible'
-        }
+
+    displayAsButton() {
+      return this.item.items.length > 0 && this.item.collapsable
     },
-    computed: {
-        collapsedByDefault() {
-            return this.item?.collapsedByDefault ?? false
-        },
+
+    collapsedByDefault() {
+      return this.item?.collapsedByDefault ?? false
     },
+  },
 }
 </script>

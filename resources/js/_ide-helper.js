45ddce5572e5400496dd6622eebfd1f47342bf41
Norman Huth <norman@huth.it>
2024-03-11 09:42:58 +0100

    refactor: remove sidebar-fa-icon from html icon

--- resources/js/_ide-helper.js
+++ resources/js/_ide-helper.js
@@ -4,7 +4,6 @@ import DropdownMenuItem from './../../../../laravel/nova/resources/js/components
 import FormButton from './../../../../laravel/nova/resources/js/components/FormButton'
 import CollapseButton from './../../../../laravel/nova/resources/js/components/CollapseButton'
 import Icon from './../../../../laravel/nova/resources/js/components/Icons/Icon.vue'
-import NHMenuIcon from '@norman-huth/helpers-collection-js/components/nova/Icon.vue'
 
 window.__ = function (key, replacements) {}
 
@@ -13,7 +12,6 @@ Vue.component('DropdownMenu', DropdownMenu)
 Vue.component('DropdownMenuItem', DropdownMenuItem)
 Vue.component('FormButton', FormButton)
 Vue.component('CollapseButton', CollapseButton)
-Vue.component('NHMenuIcon', NHMenuIcon)
 Vue.component('Icon', Icon)
 Vue.mixin({
   methods: {
